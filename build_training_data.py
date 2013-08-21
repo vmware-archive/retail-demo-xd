5c68780cf0d33a86ed7dcf933481bfc2f5d1de52
Adam Zwickey <azwickey@C02JW12SDKQ5-3.local>
2013-08-21 16:00:41 -0400

    Adding logistic regression

---
+++ build_training_data.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+
+import re
+import shutil
+import commands
+import random
+import json
+import urllib2
+
+def main():
+  for x in xrange(100001):
+    cust_id = str(x)
+    order_id = str(random.randrange(2000,10000))
+    order_amount = str('{:20.2f}'.format(random.uniform(100,10000))).strip()
+    store_id = str(random.randrange(1,1000))
+
+    if x % 500 == 0:
+       cust_id = "16186961"
+       
+    num_items = str(random.randrange(10,1000))
+    
+    is_fraudulent = 0;
+    # Flag items above a specific $$ per item and specfic stores
+    if ((float(order_amount)/int(num_items)) >= 500) and (int(store_id) % 10 == 0):
+        is_fraudulent = 1;
+        print "************************"
+        print "Created fraudulent order"
+        print "************************"   
+       
+    data = json.dumps({"customerid": cust_id, "orderid": order_id, "orderamount": order_amount, "storeid": store_id, "items": num_items, "fraudulent": is_fraudulent})
+    
+    req = urllib2.Request("http://localhost:8001", data, {'Content-Type': 'application/json'})
+    #print "POST URL:" + req.get_full_url()
+    #print "POST DATA:" + req.data
+    f = urllib2.urlopen(req)
+    response = f.read()
+    f.close()
+  
+if __name__ == "__main__":
+  main()
