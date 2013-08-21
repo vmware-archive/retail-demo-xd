5b2c2340ba7ba9b57ac2f8e9cf64fd26d3ff2466
Adam Zwickey <azwickey@C02JW12SDKQ5-3.local>
2013-08-21 15:59:07 -0400

    Adding logistic regression

---
+++ springxd_files/modules/processor/scripts/order-transformer-training.groovy
@@ -0,0 +1,10 @@
+/**
+ * Takes a JSON payload and converts it into
+ * a pipe delimited String.
+ *
+ */
+import groovy.json.JsonSlurper
+ 
+def jsonOrder = new JsonSlurper().parseText( payload )
+def pipeString = jsonOrder.customerid + "|" + jsonOrder.orderid + "|" + jsonOrder.orderamount + "|" + jsonOrder.storeid + "|" + jsonOrder.items + "|" + jsonOrder.fraudulent
+return pipeString
