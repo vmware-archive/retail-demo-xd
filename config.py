3844573bbc11ec226813b46ae14a5fbb0d3629f4
James Williams <cfossguy@gmail.com>
2013-09-03 21:32:42 -0400

    added a very simple batch job example. executes every 5 seconds. only does an ls on hdfs.

--- config.py
+++ config.py
@@ -10,8 +10,10 @@ sqlf_hostname='sqlfire'
 sqlf_home='/home/gpadmin/vFabric_SQLFire_11_b40332'
 
 #Locator client port setting. 
-sqlf_client_port='1527'
+sqlf_client_port='7711'
 
 #Location of the Spring XD binary home 
-springxd_home='/Users/alstom/Development/pivotalvfabric/spring-xd/build/dis/spring-xd/xd'
+springxd_home='/Users/williamsj/hadoop/PivotalHD/SpringXD/spring-xd-1.0.0.M2/'
+
+
 
